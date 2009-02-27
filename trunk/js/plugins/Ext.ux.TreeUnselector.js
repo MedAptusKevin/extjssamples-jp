3b77501ced5ba7335f0059dace75c4e27365cb51
naotori <naotori@c15411ec-047b-11de-994d-8308d8631604>
2009-02-27 05:55:22 +0000

    Added Ext.ux.TreeUnselector plugin sample

---
+++ trunk/js/plugins/Ext.ux.TreeUnselector.js
@@ -0,0 +1,26 @@
+/**
+ * A treepanel plugin to enable "unselecting" tree node  
+ */
+
+Ext.ns('Ext.ux');
+
+Ext.ux.TreeUnselector = function(){
+  this.init = function(tree){
+
+    tree.on('render',function(tree){
+      var el = tree.getTreeEl();
+			el.on('click',function(e,t){ 
+			  if(this.id == t.id){ 
+					tree.getSelectionModel().clearSelections(); 
+				} 
+			},el); 
+		}); 
+
+		tree.on('beforeclick',function(node,event){ 
+			if(node.isSelected()){ 
+				node.unselect(); 
+				return false; 
+			}
+		});
+	};    
+};
