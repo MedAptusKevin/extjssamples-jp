630ef42b537e4d9df37935f32d75647ccf60ad4f
naotori <naotori@c15411ec-047b-11de-994d-8308d8631604>
2009-03-12 11:59:29 +0000

    Fixed to work on Mac

--- js/Ext.ux.SpreadsheetGridPanel.js
+++ js/Ext.ux.SpreadsheetGridPanel.js
@@ -22,41 +22,43 @@ Ext.ux.SpreadsheetGridPanel = Ext.extend(Ext.grid.EditorGridPanel,{
     var  fields = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'.split(' ').slice(0,this.colsNum);
     var that = this;
 
-    var editorCfg = {
-      enableKeyEvents: true,
-      listeners: {
-        keyup: {
-          fn: function(t,e){
-            if(e.ctrlKey && e.getKey()==86){
-              (function(){
-                var v = t.getRawValue();
-                t.setRawValue('');
-                var rows = v.split("\n");
-                
-                var cells = [];
-                for(var i = 0; i < rows.length; i++){
-                  cells[i] = rows[i].split("\t"); 
-                }
-                var g = that;
-                var cm = g.getColumnModel();
-                var store = that.getStore();
-                var selected = g.getSelectionModel().getSelectedCell();
-                var row = selected ? selected[0]: 0;
-                var col = selected ? selected[1]: 0;
-
-                for(var i=0; i<cells.length; i++){
-                  var rec = store.getAt(row+i);
-                  for(var j=0; j<cells[i].length; j++){
-                    var fieldName = cm.getDataIndex(col+j);
-                    rec.set(fieldName,cells[i][j]);
-                  }
-                }
-                t.hide();
-              }).defer(100);
+		var ev = Ext.isMac ? 'keypress' : 'keyup';
+		var code = Ext.isMac ? 118 : 86;
+		var listener = {};
+		listener[ev] = {fn: function(t,e){
+        if(e.ctrlKey && e.getKey()==code){
+          (function(){
+            var v = t.getRawValue();
+            t.setRawValue('');
+            var rows = v.split("\n");
+            
+            var cells = [];
+            for(var i = 0; i < rows.length; i++){
+              cells[i] = rows[i].split("\t"); 
             }
-          }
+            var g = that;
+            var cm = g.getColumnModel();
+            var store = that.getStore();
+            var selected = g.getSelectionModel().getSelectedCell();
+            var row = selected ? selected[0]: 0;
+            var col = selected ? selected[1]: 0;
+
+            for(var i=0; i<cells.length; i++){
+              var rec = store.getAt(row+i);
+              for(var j=0; j<cells[i].length; j++){
+                var fieldName = cm.getDataIndex(col+j);
+                rec.set(fieldName,cells[i][j]);
+              }
+            }
+            t.hide();
+          }).defer(100);
         }
       }
+    };
+
+    var editorCfg = {
+      enableKeyEvents: true,
+      listeners: listener 
     };
 
     var columns = [], data = [];
