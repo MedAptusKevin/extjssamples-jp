53a7855d869cd049543152d15610c96921612f27
naotori <naotori@c15411ec-047b-11de-994d-8308d8631604>
2009-05-26 16:50:05 +0000

    added Ext.ux.GridDDText.js and sample

---
+++ js/plugins/Ext.ux.GridDDText.js
@@ -0,0 +1,36 @@
+/**
+ * A gridpanel plugin to customize ddText
+ */
+
+Ext.ns('Ext.ux');
+
+Ext.ux.GridDDText = function(){
+  this.init = function(grid){
+    this.grid = grid;
+    grid.on('render',this.modifyDragDropText, this);
+  },
+
+  this.modifyDragDropText = function(){
+    var grid = this.grid;
+    var v = grid.getView();
+    if(!v.dragZone) return;
+
+    grid.getDragDropText = function(){
+      var sel = v.dragZone.dragData.selections;
+      var rows = [];
+      for(var i=0; i<sel.length; i++){
+        var data = [];
+        for(var key in sel[i].data){
+          data.push(sel[i].data[key]);
+        }
+        if(i>=2){
+          rows.push('...(その他'+(sel.length-2)+'行)');
+          break;
+        }else{
+          rows.push(data.join(','));
+        }
+      }
+      return rows.join('<br />');
+		};
+  }
+};
